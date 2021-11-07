9b9957ca9dca971cd8ac52723f69eb6c9d2634fd
Lucas Seeger <seegerlucas295@gmail.com>
2021-11-06 18:33:20 -0600

    added users

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # node-express-api-tutorial
+
+following this turotial https://www.youtube.com/watch?v=l8WPWK9mS5M&t=737s
+
+uses node packages:
+express
+nodemon
+
+
+after cloning repository:
+
+$ cd node-express-api-tutorial 
+$ npm install 
---
+++ src/routes/users.js
@@ -0,0 +1,29 @@
+import  express  from "express";
+
+const router = express.Router();
+
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+]
+
+//all routes in here are starting with /users
+router.get('/', (req, res) => {
+    console.log(users)
+    res.send(users)
+});
+
+router.post('/', (req, res) => {
+    
+});
+
+
+export default router
