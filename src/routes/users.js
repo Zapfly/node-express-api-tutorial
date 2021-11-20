76ea8a8b25f06890a3d27e9b9edb2c05eb75ee5e
Lucas Seeger <seegerlucas295@gmail.com>
2021-11-20 07:32:43 -0700

    continued refactoring

--- src/routes/users.js
+++ src/routes/users.js
@@ -29,8 +29,6 @@ router.delete('/:id', (req, res) => {
 router.patch('/:id', (req, res) => {
     const { id } = req.params;
     const { firstName, lastName, age} = req.body;
-    // const firstName = req.body.firstName;
-    // const lastName = req.body.lastName
 
     const user = users.find((user) => user.id === id);
 
