e89f11c9fc8dbd9b76da1c6097a0bd3d0a861181
Lucas Seeger <seegerlucas295@gmail.com>
2021-11-20 07:32:14 -0700

    began refactoring

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,6 @@
-import {users} from '../routes/users';
+import express from "express";
 import {v4 as uuidv4} from 'uuid';
-
+import { users } from '../routes/users.js'
 
 export const createUser = (req, res) => {
     const user = req.body;
@@ -8,5 +8,12 @@ export const createUser = (req, res) => {
     users.push({ ...user, id: uuidv4()});
 
     res.send(`User with the name ${user.firstName} added to DB`);
+}
 
-};
+export const getUser = (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,34 +1,22 @@
 import  express  from "express";
-import {v4 as uuidv4} from 'uuid';
+
+import { createUser, getUser } from "../controllers/users.js";
 
 const router = express.Router();
 
-let users = [];
+export let users = [];
 
 //all routes in here are starting with /users
 
 router.get('/', (req, res) => {
-    console.log(users)
     res.send(users)
 });
 
 
-router.post('/', (req, res) => {
-    const user = req.body;
 
-    users.push({ ...user, id: uuidv4()});
+router.post('/', createUser);
 
-    res.send(`User with the name ${user.firstName} added to DB`);
-
-});
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
-
-});
+router.get('/:id', getUser);
 
 router.delete('/:id', (req, res) => {
     const {id} =  req.params;
@@ -41,6 +29,8 @@ router.delete('/:id', (req, res) => {
 router.patch('/:id', (req, res) => {
     const { id } = req.params;
     const { firstName, lastName, age} = req.body;
+    // const firstName = req.body.firstName;
+    // const lastName = req.body.lastName
 
     const user = users.find((user) => user.id === id);
 
